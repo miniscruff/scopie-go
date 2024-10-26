ab7bea0a530d07aaa98faf73d8b5e573f3c30404
Ronnie <miniscruff@hey.com>
2024-10-26 11:12:05 -0700

    Support the alpha 02 changes from scopie (#9)

--- scopie_test.go
+++ scopie_test.go
@@ -10,12 +10,12 @@ import (
 )
 
 type testAllowedScenario struct {
-	ID        string            `json:"id"`
-	Actor     string            `json:"actor"`
-	Scopes    string            `json:"scopes"`
-	Result    bool              `json:"result"`
-	Variables map[string]string `json:"variables"`
-	Error     string            `json:"error"`
+	ID           string            `json:"id"`
+	ActorRules   []string          `json:"actorRules"`
+	ActionScopes []string          `json:"actionScopes"`
+	Result       bool              `json:"result"`
+	Variables    map[string]string `json:"variables"`
+	Error        string            `json:"error"`
 }
 
 type testValidScenario struct {
@@ -52,7 +52,7 @@ func TestMain(m *testing.M) {
 func Test_IsAllowed(t *testing.T) {
 	for _, scenario := range testCases.IsAllowedTests {
 		t.Run(scenario.ID, func(t *testing.T) {
-			res, err := IsAllowed(scenario.Variables, scenario.Scopes, scenario.Actor)
+			res, err := IsAllowed(scenario.ActionScopes, scenario.ActorRules, scenario.Variables)
 			if scenario.Error != "" {
 				then.NotNil(t, err)
 				then.Equals(t, scenario.Error, err.Error())
@@ -62,11 +62,14 @@ func Test_IsAllowed(t *testing.T) {
 			}
 		})
 	}
+}
 
+func Test_IsAllowedBenchmarks(t *testing.T) {
 	// Also run our benchmarks as test cases separate from running benchmarks
 	for _, scenario := range testCases.Benchmarks {
 		t.Run(scenario.ID, func(t *testing.T) {
-			_, err := IsAllowed(scenario.Variables, scenario.Scopes, scenario.Actor)
+			res, err := IsAllowed(scenario.ActionScopes, scenario.ActorRules, scenario.Variables)
+			then.Equals(t, scenario.Result, res)
 			then.Nil(t, err)
 		})
 	}
@@ -86,127 +89,78 @@ func Test_ScopeValid(t *testing.T) {
 	}
 }
 
-func Test_JumpAfterSep_WhenFound(t *testing.T) {
-	value := "hello_world"
-	afterIndex := jumpAfterSeperator(&value, 0, '_')
-	then.Equals(t, 6, afterIndex)
-	then.Equals(t, 'w', value[afterIndex])
+type compareTestCase struct {
+	name   string
+	actor  string
+	action string
+	vars   map[string]string
+	err    error
+	res    bool
 }
 
-func Test_JumpAfterSep_WhenNotFound(t *testing.T) {
-	value := "hello_world"
-	afterIndex := jumpAfterSeperator(&value, 0, '$')
-	then.Equals(t, 11, afterIndex)
-}
+func Test_CompareActorToRule(t *testing.T) {
+	for _, tc := range []compareTestCase{
+		{
+			name:   "basic equality",
+			actor:  "allow/alpha/beta",
+			action: "alpha/beta",
+			res:    true,
+		},
+		{
+			name:   "first inequality",
+			actor:  "allow/alpha/beta",
+			action: "delta/beta",
+			res:    false,
+		},
+		{
+			name:   "last inequality",
+			actor:  "allow/alpha/beta/ceti/delta",
+			action: "alpha/beta/ceti/epsilon",
+			res:    false,
+		},
+		{
+			name:   "wildcard equality",
+			actor:  "allow/alpha/beta/*/delta",
+			action: "alpha/beta/ceti/delta",
+			res:    true,
+		},
+		{
+			name:   "super wildcard equality",
+			actor:  "allow/alpha/beta/**",
+			action: "alpha/beta/ceti/delta",
+			res:    true,
+		},
+		{
+			name:   "variable usage",
+			actor:  "allow/alpha/@user",
+			action: "alpha/our_user",
+			vars: map[string]string{
+				"user": "our_user",
+			},
+			res: true,
+		},
+		{
+			name:   "first array value",
+			actor:  "allow/alpha/beta|ceti|delta",
+			action: "alpha/beta",
+			res:    true,
+		},
+		{
+			name:   "last array value",
+			actor:  "allow/alpha/beta|ceti|delta",
+			action: "alpha/delta", // last array value of epsilon
+			res:    true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
 
-func Test_JumpBlockOrScope_WhenBlock(t *testing.T) {
-	value := "alpha/beta/ceti"
-	afterIndex := jumpBlockOrScopeSeperator(&value, 0)
-	then.Equals(t, 6, afterIndex)
-}
-
-func Test_JumpBlockOrScope_WhenScope(t *testing.T) {
-	value := "alpha,beta,ceti"
-	afterIndex := jumpBlockOrScopeSeperator(&value, 0)
-	then.Equals(t, 6, afterIndex)
-}
-
-func Test_JumpBlockOrScope_WhenNeitherScopeOrBlock(t *testing.T) {
-	value := "alphabetaceti"
-	afterIndex := jumpBlockOrScopeSeperator(&value, 0)
-	then.Equals(t, 13, afterIndex)
-}
-
-func Test_CompareStringsAfterIndexes_WithMatch(t *testing.T) {
-	a := "allow/alpha/beta"
-	b := "alpha/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, nil)
-	then.True(t, doesMatch)
-	then.Equals(t, 12, nextA)
-	then.Equals(t, 6, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_NoMatch(t *testing.T) {
-	a := "allow/alpha/beta"
-	b := "centi/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, nil)
-	then.False(t, doesMatch)
-	then.Equals(t, 6, nextA)
-	then.Equals(t, 0, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_DiffLengths(t *testing.T) {
-	a := "allow/alpha/beta"
-	b := "unicorn/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, nil)
-	then.False(t, doesMatch)
-	then.Equals(t, 6, nextA)
-	then.Equals(t, 0, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_AtEnd(t *testing.T) {
-	a := "allow/alpha/beta"
-	b := "alpha/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 12, &b, 6, nil)
-	then.True(t, doesMatch)
-	then.Equals(t, len(a)+1, nextA)
-	then.Equals(t, len(b)+1, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_WithWildcard(t *testing.T) {
-	a := "allow/*/beta"
-	b := "alpha/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, nil)
-	then.True(t, doesMatch)
-	then.Equals(t, 8, nextA)
-	then.Equals(t, 6, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_WithArrays(t *testing.T) {
-	a := "allow/alpha|beta|ceti|omega|tango/beta"
-	b := "omega/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, nil)
-	then.True(t, doesMatch)
-	then.Equals(t, 34, nextA)
-	then.Equals(t, 6, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_WithArraysInList(t *testing.T) {
-	a := "allow/alpha|beta|ceti|omega|tango,allow/alpha/delta"
-	b := "omega/beta"
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, nil)
-	then.True(t, doesMatch)
-	then.Equals(t, 34, nextA)
-	then.Equals(t, 6, nextB)
-	then.Nil(t, err)
-}
-
-func Test_CompareStringsAfterIndexes_WithVar(t *testing.T) {
-	a := "allow/@me/beta"
-	b := "omega/beta"
-	vars := map[string]string{
-		"me": "omega",
-	}
-	nextA, nextB, doesMatch, err := compareFrom(&a, 6, &b, 0, vars)
-	then.True(t, doesMatch)
-	then.Equals(t, 10, nextA)
-	then.Equals(t, 6, nextB)
-	then.Nil(t, err)
-}
-
-func Benchmark_Validations(b *testing.B) {
-	for _, scenario := range testCases.Benchmarks {
-		b.Run(scenario.ID, func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				_, err := IsAllowed(scenario.Variables, scenario.Scopes, scenario.Actor)
-				then.Nil(b, err)
+			doesMatch, err := compareActorToAction(&tc.actor, &tc.action, tc.vars)
+			if tc.err == nil {
+				then.Nil(t, err)
+				then.Equals(t, tc.res, doesMatch)
+			} else {
+				then.Err(t, tc.err, err)
 			}
 		})
 	}
